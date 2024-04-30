79953b3eeeacd795cfec12ef217f9d30c9f3cd9e
tn00627974 <139155210+tn00627974@users.noreply.github.com>
2024-04-30 23:20:12 +0800

    新增MySQL資料庫

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = "firstproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        # mysql
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_data",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
