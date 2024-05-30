67bcb83082c9f304eb899442758baf5819687a8a
tn00627974 <139155210+tn00627974@users.noreply.github.com>
2024-05-30 10:07:44 +0800

    BUG fix

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -23,9 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls), # path(網址, 函式)
     path('', index,name='index'), # 首頁 顯示所有文章
     path('blog_post_detail/<int:post_id>/',blog_post_detail,name='blog_post_detail'),
-
-    path('Portfolio/',portfolio,name='portfolio'), # 文章 顯示所有文章 
-    path('about/',about,name='about'), # 文章 顯示所有文章 
+    path('Portfolio/',portfolio,name='portfolio'), # 作品集
+    path('about/',about,name='about'), # 關於我 
     
     # Blog 文章
     # path('blog/',blog,name='blog'), # 文章 顯示所有文章 
