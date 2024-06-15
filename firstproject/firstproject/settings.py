59b4b678c98fca3a2e9f384c0f9beb55641a398b
redroom <139155210+tn00627974@users.noreply.github.com>
2024-06-15 20:23:33 +0800

    whitenoise

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -173,6 +174,7 @@ STATICFILES_DIRS = [ # 加入 static 路徑
     # BASE_DIR /'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles') # 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # 靜態檔案壓縮
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
