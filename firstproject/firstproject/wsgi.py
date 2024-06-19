c652d17dc168ab213d440a9b92eef66e9f9de399
redroom <139155210+tn00627974@users.noreply.github.com>
2024-06-19 09:01:24 +0800

    gunicorn

--- firstproject/firstproject/wsgi.py
+++ firstproject/firstproject/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-# import os
+import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firstproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firstproject.settings")
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
