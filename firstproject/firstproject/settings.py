c5ee45e55d2a756fe9b1aeb75cd8d6f907f032cf
Redmoon <139155210+tn00627974@users.noreply.github.com>
2024-06-19 08:41:40 +0800

    Update settings.py

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -184,7 +184,7 @@ STATICFILES_DIRS = [ # 加入 static 路徑
     os.path.join(BASE_DIR, 'static'), # 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles') # 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # 靜態檔案壓縮
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # 靜態檔案壓縮
 
 
 # Default primary key field type
